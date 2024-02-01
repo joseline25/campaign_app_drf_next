9cc0a0cfcab3aae0d6adab2292b5f7f6310f446c
joseline25 <youjoseline@gmail.com>
2024-02-01 16:12:34 +0100

    fetching all the campaigns from the server

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:ital,wght@1,300;1,400&family=Nunito+Sans:ital,opsz@1,6..12&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
